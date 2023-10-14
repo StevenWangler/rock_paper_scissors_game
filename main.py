7e275c1cf9cbecf18a57785dd2d7842517c2505e
Steven Wangler <wangler.steven@outlook.com>
2023-10-14 16:43:13 -0400

    Minor formatting changes

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ Rock, Paper, Scissors Game Module.
 
 This module allows a user to play Rock, Paper, Scissors against a "learning" computer opponent.
 The computer's strategy revolves around targeting the player's most defeated move, with a slight 
-random twist to 
-introduce unpredictability.
+random twist to introduce unpredictability.
 
 Developer: Steven Wangler
 """
@@ -13,12 +12,15 @@ import time
 import random
 from colorama import init, Fore
 
+
 # Initialize colorama for colorful terminal outputs
 init(autoreset=True)
+
 
 # Constants
 MOVES = ['rock', 'paper', 'scissors']
 DELIMITER = '~' * 50
+
 
 # Mapping of moves to the move they defeat
 WINNING_COMBINATIONS = {
@@ -26,6 +28,7 @@ WINNING_COMBINATIONS = {
     'scissors': 'paper',
     'paper': 'rock'
 }
+
 
 # ASCII arts for visual representation of each move
 ASCII_ART = {
@@ -55,12 +58,14 @@ ASCII_ART = {
 """
 }
 
+
 def display_intro():
     """Display the introductory message for the game."""
     print(DELIMITER)
     print(Fore.CYAN + 'Welcome to Rock - Paper - Scissors. Can you beat a learning computer?')
     print(Fore.YELLOW + "Press 'R' for Rock, 'P' for Paper, 'S' for Scissors, and 'Q' to quit.")
     print(DELIMITER)
+
 
 def get_computer_choice(defeat_counts):
     """
@@ -79,6 +84,7 @@ def get_computer_choice(defeat_counts):
     most_defeated_move = max(defeat_counts, key=defeat_counts.get)
     return WINNING_COMBINATIONS[most_defeated_move]
 
+
 def get_player_choice():
     """Get player's move choice."""
     choice = input(Fore.GREEN + "Your move (R/P/S/Q): ").lower()
@@ -89,12 +95,14 @@ def get_player_choice():
         'q': 'quit'
     }.get(choice, 'invalid')
 
+
 def reveal_choices(player, computer):
     """Display both the player and computer's chosen moves."""
     print("\nRock... Paper... Scissors... SHOOT!\n")
     time.sleep(1)
     print(Fore.BLUE + "You chose:\n" + ASCII_ART[player])
     print(Fore.RED + "Computer chose:\n" + ASCII_ART[computer])
+
 
 def determine_winner(player_choice, computer_choice):
     """
@@ -114,13 +122,14 @@ def determine_winner(player_choice, computer_choice):
     else:
         return "computer"
 
+
 def main():
     """Main game loop."""
-    COMPUTER_WINS = 0
-    HUMAN_WINS = 0
-    TIES = 0
+    computer_wins = 0
+    human_wins = 0
+    ties = 0
     defeat_counts = {'rock': 0, 'paper': 0, 'scissors': 0}
-    
+ 
     display_intro()
 
     # Game loop
@@ -139,23 +148,24 @@ def main():
         result = determine_winner(player_choice, computer_choice)
         if result == "tie":
             print(Fore.MAGENTA + "It's a tie!")
-            TIES += 1
+            ties += 1
         elif result == "player":
             print(Fore.BLUE + "You've won this round!")
-            HUMAN_WINS += 1
+            human_wins += 1
             defeat_counts[computer_choice] += 1
         else:
             print(Fore.RED + "The computer wins this round!")
-            COMPUTER_WINS += 1
+            computer_wins += 1
             defeat_counts[player_choice] += 1
 
         print(DELIMITER)
-        print(f'{Fore.CYAN}Score:\n{Fore.BLUE}You: {HUMAN_WINS}\n{Fore.RED}Computer: {COMPUTER_WINS}\n{Fore.MAGENTA}Ties: {TIES}')
+        print(f'{Fore.CYAN}Score:\n{Fore.BLUE}You: {human_wins}\n{Fore.RED}Computer: {computer_wins}\n{Fore.MAGENTA}Ties: {ties}')
         print(DELIMITER)
 
     print(Fore.CYAN + "Thanks for playing! Here's the final score:")
-    print(f'{Fore.BLUE}You: {HUMAN_WINS} | {Fore.RED}Computer: {COMPUTER_WINS} | {Fore.MAGENTA}Ties: {TIES}')
+    print(f'{Fore.BLUE}You: {human_wins} | {Fore.RED}Computer: {computer_wins} | {Fore.MAGENTA}Ties: {ties}')
     print(DELIMITER)
+
 
 if __name__ == '__main__':
     main()
